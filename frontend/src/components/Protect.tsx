5dfef846b1120557a7a8d59e7616bee35837d17e
kishan1735 <gswwarriorsnba@gmail.com>
2024-07-08 22:52:01 +0530

    fix: fix protected routes

--- frontend/src/components/Protect.tsx
+++ frontend/src/components/Protect.tsx
@@ -1,10 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Outlet } from "react-router-dom";
+import toast from "react-hot-toast";
+import { Navigate, Outlet } from "react-router-dom";
 
 function Protect({ type }) {
-	const [protect, setProtect] = useState(false);
+	const [protect, setProtect] = useState(true);
 	const { data, status } = useQuery({
 		queryKey: ["type"],
 		queryFn: () => {
@@ -13,23 +14,22 @@ function Protect({ type }) {
 			});
 		},
 	});
-	console.log(data);
 	useEffect(() => {
 		if (
 			status === "success" &&
 			data.data.status === "success" &&
 			data.data.type === type
 		) {
-			console.log(data);
 			setProtect(true);
+		} else if (
+			status === "success" &&
+			(data.data.status === "failed" || data.data.type !== type)
+		) {
+			setProtect(false);
+			toast.error("Login and try again");
 		}
 	}, [data, setProtect, status, type]);
-	if (protect)
-		return (
-			<>
-				<Outlet />
-			</>
-		);
+	return protect ? <Outlet /> : <Navigate to={`/${type}/login`} />;
 }
 
 export default Protect;
